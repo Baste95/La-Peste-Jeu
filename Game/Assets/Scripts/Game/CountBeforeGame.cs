d4dec9f0b417e8a93a6e059a67d97139e7adf2b1
Baste95 <44084317+Baste95@users.noreply.github.com>
2020-11-20 19:19:12 +0100

    Update

---
+++ Game/Assets/Scripts/Game/CountBeforeGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountBeforeGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Time
+    public Text countText;
+    private float timeLeft = 3f;
+    public GameObject scoreGO;
+    public GameObject timeGO;
+    public Player Player;
+
+    void Start()
+    {
+        //Time.timeScale = 0;
+        scoreGO.SetActive(false);
+        timeGO.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeLeft <= 0.5)
+        {
+            scoreGO.SetActive(true);
+            timeGO.SetActive(true);
+            Destroy(countText);
+            Player.isGameOver = false;
+            //Time.timeScale = 1;
+            Destroy(this);
+        }
+        DecreaseTime();
+    }
+
+    private void DecreaseTime()
+    {
+        timeLeft -= Time.deltaTime;
+        countText.text = Mathf.Round(timeLeft) + "";
+    }
+}
